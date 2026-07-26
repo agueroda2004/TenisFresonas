ae4d3e849a970cd8173ea77108fc817369c6196d
Daniel Aguero <139009647+agueroda2004@users.noreply.github.com>
2026-07-26 16:51:20 -0600

    Add additional product brands to constants
    Added new product brands to the PRODUCT_BRANDS constant.

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -2,4 +2,10 @@ export const PRODUCT_TYPES = [{ value: "tenis", label: "Tenis" }];
 export const PRODUCT_BRANDS = [
   { value: "nike", label: "Nike" },
   { value: "adidas", label: "Adidas" },
+  { value: "newBalance", label: "New Balance" },
+  { value: "jordan", label: "Jordan" },
+  { value: "asics", label: "Asícs" },
+  { value: "puma", label: "Puma" },
+  { value: "converse", label: "Converse" },
+  { value: "vans", label: "Vans" },
 ];
